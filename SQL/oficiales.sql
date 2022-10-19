4bdfd55f95c8899d888357dcb1894b4a31fa5022
1720110203 <78154372+1720110203@users.noreply.github.com>
2022-10-19 09:39:23 -0500

    base de datos creada

--- SQL/oficiales.sql
+++ SQL/oficiales.sql
@@ -1,14 +1,14 @@
 CREATE TABLE oficiales (
-    id_oficial INT PRIMARY  KEY AUTOINCREMENT,
-    nombre str,
-    apellidoPaterno str,
-    apellidoMaterno str,
-    cargo_grado str,
-    abscripcion str,
-    num_patrulla str,
-    pass str,
+    id_oficial INTEGER PRIMARY KEY AUTOINCREMENT NOTNULL,
+    nombre varchar (30) NOTNULL,
+    apellido_paterno varchar (30) NOTNULL,
+    apellido_materno varchar (30) NOTNULL,
+    cargo_grado varchar (50) NOTDEFAULT,
+    abscripcion varchar (50) NOTNULL,
+    num_patrulla varchar (30) NOTNULL,
+    passw varchar (30)NOTNULL,  
 
 );
 
-INSERT INTO nombre, apellidoPaterno, apellidoMaterno, cargo_grado, abscripcion, num_patrulla, pass VALUES 
-("Federico", "Hernandez", "Leyva", "Administrativa", "Subalterno","abscripcion")
+INSERT INTO oficiales (nombre, apellido_paterno, apellido_materno, cargo_grado, abscripcion, num_patrulla, passw) VALUES 
+("Federico", "Hernandez", "Leyva", "Administrativa", "Subalterno","Politica","2564_Ab", "IHbyg3nj");
