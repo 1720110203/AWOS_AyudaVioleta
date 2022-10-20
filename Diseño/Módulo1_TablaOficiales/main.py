d200cbf90b6f0d88a5cf6145e4ac0752d5053c69
1720110203 <78154372+1720110203@users.noreply.github.com>
2022-10-20 17:00:04 -0500

    Oficiales

--- Diseño/Módulo1_TablaOficiales/main.py
+++ Diseño/Módulo1_TablaOficiales/main.py
@@ -11,21 +11,20 @@ class Mensaje(BaseModel):
     mensaje:str
 
 class Oficial(BaseModel):
-    id_oficial: int
+    id_oficial : int
     nombre : str
-    apellido1 : str
-    apellido2 : str
-    carga : str 
+    apellido_paterno : str
+    apellido_materno : str
+    cargo_grado : str
     abscripcion : str
     num_patrulla : str
-    contraseña : str
-
+    passw : str
 
 class OficialIN(BaseModel):
     nombre : str
-    apellidoPaterno : str
-    apellidoMaterno : str
-    cargo_grado : str 
+    apellido_paterno : str
+    apellido_materno : str
+    cargo_grado : str
     abscripcion : str
     num_patrulla : str
     passw : str
@@ -45,7 +44,7 @@ app = FastAPI (
     contact  = {
         "name": "pilarlm",
         "url" : "http://github.com/1720110203",
-        "email": "1720110203@utectulancingo.edu",
+        "email": "1720110203@utectulancingo.edu.mx",
     },
     license_info = {
         "name": "Apache 2.0",
@@ -74,7 +73,7 @@ async def get_root():
     response_model = List [Oficial], #lista
     status_code = status.HTTP_202_ACCEPTED,
     summary = "Lista de contactos",
-    description = "Endpoint que regresa un array con todos los contactos"
+    description = "Endpoint que regresa un array con todos los oficiales"
 )
 
 async def get_contactos():
@@ -91,5 +90,37 @@ async def get_contactos():
             status_code = status.HTTP_400_BAD_REQUEST, #mostrara
             detail="Error al consultar datos" #mostrara
         )
-       
+
+#------------------------------------------------------------------------------------
+#METODO PARA CONSULTAR UN SOLO CONTACTO
+
+@app.get (
+    "/oficiales/{id_oficial}", #inicia
+    response_model = Oficial, #lista
+    status_code = status.HTTP_202_ACCEPTED,
+    summary = "Oficial",
+    description = "Endpoint que registra un oficial"
+)  
+async def get_contactos(id_oficial : int):
+    try:
+        with sqlite3.connect("SQL/ayudaV.db") as connection: #conecta a mi base
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            sqlV =  "SELECT * FROM oficiales WHERE id_oficial = ?;" #consulta atraves del id
+            values = (id_oficial,)
+            cursor.execute (sqlV,values)#se ejecutaran en tupla
+            response = cursor.fetchone() #uno solo
+            if response == None: #en caso de error al no mostrar el id encontrado
+                return JSONResponse (status_code=404, content= {"mensaje":"Sin registro del oficial"}) #Estatus de no encontrado 
+            else: 
+                return response
+    except Exception as error:  #en caso de error
+        print (f"error interno : {error.args}")
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST, #mostrara
+            detail="Error al consultar datos" #mostrara
+        )
+
+#----------------------------------------------------------------------------
+#METODO PARA INSERTAR UN OFICIAL 
 
